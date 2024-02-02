fc0515eb7c8cc0c25106413f5052f5718ca27d23
Peter Odero <peterodero561@gmail.com>
2024-02-02 09:33:02 +0000

    build: all: created a script that checks for one or more occurrences of "t" in commandline arguments

---
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+# A script that checks a word repettion
+
+def match_word(arg)
+  regex = /hbt+n/
+  matches = arg.scan(regex)
+  result = matches.join
+  puts result
+end
+
+# check commandline arguments
+
+if ARGV.empty?
+  puts "Usage: #{$PROGRAM_NAME} <argument>"
+else
+  argument = ARGV[0]
+  match_word(argument)
+end
