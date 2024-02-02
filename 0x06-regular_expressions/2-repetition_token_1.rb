381294d24c9990c4c782ae414ee4ecbe200524d0
Peter Odero <peterodero561@gmail.com>
2024-02-02 09:18:51 +0000

    build: all: created a script that check for zero or one occurrences of "b"

---
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+# A script that serch for  0 or 1 "b" in a pattern
+
+def match(arg)
+  regex = /hb?tn/
+  matches = arg.scan(regex)
+  result = matches.join
+  puts result
+end
+
+#check for commandline arguments
+
+if ARGV.empty?
+  puts "Usage: #{$PROGRAM_NAME} <argument>"
+else
+  args = ARGV[0]
+  match(args)
+end
