603d4c2516b3937584ff7b92a5d607fd555cec9c
Peter Odero <peterodero561@gmail.com>
2024-02-02 10:24:12 +0000

    build: all: created a script that extracts sms info from a log file

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,13 +1,26 @@
 #!/usr/bin/env ruby
 # A script that checks a 10 numbers exactly
 
-def extract(arg)
-  regex = /\[from:([^\]]*)\] \[to:([^\]]*)\] \[flags:([^\]]*)\]/
-  match_data = arg.match(regex)
-  sender = match_data[1]
-  receiver = match_data[2]
-  flags = match_data[3]
-  puts "#{sender}, #{receiver}, #{flags}"
+def extract(log_entry)
+  # Define a regex pattern to extract relevant information
+  pattern = /\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/
+
+  # Use the pattern to find matches in the log entry
+  match = log_entry.match(pattern)
+
+  if match
+    sender = match[1]
+    receiver = match[2]
+    flags = match[3]
+
+    # Remove any non-alphanumeric characters from sender and receiver
+    sender.gsub!(/[^a-zA-Z0-9+]/, '')
+    receiver.gsub!(/[^a-zA-Z0-9+]/, '')
+
+    return "#{sender},#{receiver},#{flags}"
+  else
+    return "Invalid log entry format"
+  end
 end
 
 # check commandline arguments
