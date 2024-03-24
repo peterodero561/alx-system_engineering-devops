d0fb17b5178c5c5feff5ebafce4837a07b01a787
peterodero561 <peterodero561@gmail.com>
2024-03-24 13:56:32 +0000

    perf changed approach to question

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
 # This pupet file manifest and installs flask
 
-package { 'install_flask_2.1.0':
-  command   => 'pip3 install flask=2.1.0',
-  path    => '/usr/bin',
-  unless  => 'pip3 show flask | grep -q "Version: 2.1.0"',
+package { 'flask':
+  ensure => '2.1.0',
+  provider => 'pip3',
 }
