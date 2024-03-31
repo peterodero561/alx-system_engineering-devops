100efcabd15a89632ba83bfda02332307a10aaf9
peterodero561 <peterodero561@gmail.com>
2024-03-31 20:48:49 +0000

    new update 3

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,8 +1,9 @@
 # puppet to set up my ssh to connect to my server without a password
-file { '/home/ubuntu/.ssh/config':
+file { '/etc/ssh/ssh_config':
   ensure  => file,
-  owner   => 'ubuntu',
-  mode    => '0600',
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
   content => "
     Host your_server_hostname
       IdentityFile ~/.ssh/school
