7d8acf4a49d3e94d67e6e455cef8d6448455b121
peterodero561 <peterodero561@gmail.com>
2024-04-04 08:52:08 +0000

    config for ppupt

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,15 +1,37 @@
 # configure my 2 webservers using puppet to return their webserver
 # install nginx package
-class { 'nginx':
+package { 'nginx':
   ensure => installed,
 }
 
-# define custom HTTP configuration
-nginx::resource::server { 'my_server':
-  listen_options => 'default_server',
-  server_name    => 'localhost',
-  location       => '/my_location',
-  add_header     => {
-    'X-Served-By' => $::hostname,
-  },
+# Define custom Nginx configuration file
+file { '/etc/nginx/conf.d/custom_headers.conf':
+  ensure  => present,
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
+  content => "\
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+
+    server_name _;
+
+    # Add custom HTTP header
+    add_header X-Served-By $hostname;
+
+    location / {
+        root /var/www/html;
+        index index.html index.htm;
+    }
+}
+",
+  require => Package['nginx'],
+}
+
+# Ensure Nginx service is running and enabled
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+  require => Package['nginx'],
 }
