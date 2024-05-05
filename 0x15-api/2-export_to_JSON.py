5e2049c0e5b318df0a8a7593d2dc1b525cde4aec
peterodero561 <peterodero561@gmail.com>
2024-05-05 07:24:54 +0000

    fix: corrected betty errors

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -3,19 +3,20 @@
 import json
 import requests
 
+
 def get_employee_todo_progress(employee_id):
     '''exports data in json'''
-    base_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+    b_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
     response = requests.get(base_url)
-    
+
     if response.status_code == 200:
         todos = response.json()
-        
+
         # Get employee name
         u_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
         user_response = requests.get(u_url)
         username = user_response.json()['username']
-        
+
         # Organize tasks for the employee
         user_tasks = []
         for todo in todos:
@@ -25,7 +26,7 @@ def get_employee_todo_progress(employee_id):
                 "username": username
             }
             user_tasks.append(task)
-        
+
         # Write JSON data to file
         filename = f"{employee_id}.json"
         with open(filename, "w") as file:
@@ -33,11 +34,12 @@ def get_employee_todo_progress(employee_id):
     else:
         print("Error: Could not retrieve data")
 
+
 if __name__ == "__main__":
     import sys
     if len(sys.argv) != 2:
         print("Usage: python script.py <employee_id>")
         sys.exit(1)
-        
+
     employee_id = int(sys.argv[1])
     get_employee_todo_progress(employee_id)
