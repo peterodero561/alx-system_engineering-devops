5741174a716ba4f2d391eef7741633d688e4204f
peterodero561 <peterodero561@gmail.com>
2024-05-05 07:17:43 +0000

    update

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+'''Python script to export data in the JSON format.'''
+import json
+import requests
+
+def get_employee_todo_progress(employee_id):
+    '''exports data in json'''
+    base_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+    response = requests.get(base_url)
+    
+    if response.status_code == 200:
+        todos = response.json()
+        
+        # Get employee name
+        u_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+        user_response = requests.get(u_url)
+        username = user_response.json()['username']
+        
+        # Organize tasks for the employee
+        user_tasks = []
+        for todo in todos:
+            task = {
+                "task": todo['title'],
+                "completed": todo['completed'],
+                "username": username
+            }
+            user_tasks.append(task)
+        
+        # Write JSON data to file
+        filename = f"{employee_id}.json"
+        with open(filename, "w") as file:
+            json.dump({employee_id: user_tasks}, file, indent=4)
+    else:
+        print("Error: Could not retrieve data")
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <employee_id>")
+        sys.exit(1)
+        
+    employee_id = int(sys.argv[1])
+    get_employee_todo_progress(employee_id)
---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+'''Python script to export data in the JSON format.'''
+import json
+import requests
+import sys
+
+
+def export_json():
+    base_url = f'https://jsonplaceholder.typicode.com/users'
+    response = requests.get(base_url)
+    if response.status_code == 200:
+        users = response.json()
+        todo_data = {}
+        for user in users:
+            user_id = user['id']
+            username = user['username']
+            t_url = f'https://jsonplaceholder.typicode.com/users/{user_id}/todos'
+            user_todos = requests.get(t_url).json()
+            user_tasks = []
+            for todo in user_todos:
+                task = {
+                        'username': username,
+                        'task': todo['title'],
+                        'completed': todo['completed']
+                        }
+                user_tasks.append(task)
+            todo_data[user_id] = user_tasks
+
+        # write to json data file
+        with open('todo_all_employees.json', 'w') as f:
+            json.dump(todo_data, f, indent=4)
+    else:
+        print("Error: Could not retrieve data")
+
+
+if __name__ == '__main__':
+    export_json()
