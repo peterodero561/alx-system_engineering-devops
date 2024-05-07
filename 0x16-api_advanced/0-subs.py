31d1255afafaf81432fd0c79261c814d905d8143
peter odero <peterodero561@gmail.com>
2024-05-07 19:39:57 +0000

    created

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+import requests
+
+def number_of_subscribers(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            subscribers = data['data']['subscribers']
+            return subscribers
+        else:
+            return 0
+    except Exception as e:
+        print("An error occurred:", e)
+        return 0
