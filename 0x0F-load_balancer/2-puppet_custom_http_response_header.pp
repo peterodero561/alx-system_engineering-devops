057c64d5d7f8936a2cb0351105e08255bc7cf958
Peter Odero <peterodero561@gmail.com>
2024-06-28 10:37:18 +0300

    perf

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,37 +1,47 @@
-# configure my 2 webservers using puppet to return their webserver
-# install nginx package
-package { 'nginx':
-  ensure => installed,
+# Configure both web servers to return their hostname
+# This manifest ensures packages are updated, Nginx is installed, and a custom configuration is applied.
+
+# Update package lists
+exec { 'update_packages':
+  command => '/usr/bin/apt update',
+  path    => '/usr/bin',
 }
 
-# Define custom Nginx configuration file
-file { '/etc/nginx/conf.d/custom_headers.conf':
-  ensure  => present,
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
-  content => "\
+# Ensure Nginx is installed
+package { 'nginx':
+  ensure  => 'present',
+  require => Exec['update_packages'],
+}
+
+# Define custom header configuration
+file { '/etc/nginx/conf.d/custom_http_response_header.conf':
+  ensure  => 'file',
+  content => template('nginx/custom_http_response_header.conf.erb'),
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+}
+
+# Ensure Nginx service is running and restarted to apply changes
+service { 'nginx':
+  ensure     => 'running',
+  enable     => true,
+  subscribe  => File['/etc/nginx/conf.d/custom_http_response_header.conf'],
+}
+
+# Template for custom Nginx configuration
+file { '/etc/puppetlabs/code/environments/production/modules/nginx/templates/custom_http_response_header.conf.erb':
+  ensure  => 'file',
+  content => @("END"),
 server {
     listen 80 default_server;
     listen [::]:80 default_server;
 
-    server_name _;
-
-    # Add custom HTTP header
-    add_header X-Served-By $hostname;
+    server_name peterdev.tech;
 
     location / {
-        root /var/www/html;
-        index index.html index.htm;
+        add_header X-Served-By <%= @::hostname %>;
     }
 }
-",
-  require => Package['nginx'],
-}
-
-# Ensure Nginx service is running and enabled
-service { 'nginx':
-  ensure  => running,
-  enable  => true,
+| END
   require => Package['nginx'],
 }
