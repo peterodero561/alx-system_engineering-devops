f004914621c50c7b78509d4dcf00bb11fc8bb9bd
peter odero <peterodero561@gmail.com>
2024-08-13 12:09:16 +0000

    build: function that querry the reddit api to get number of subbscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# Function to query Reddit API and return no of subscribers
+import requests
+
+def number_of_subscribers(subreddit):
+    """Query reddit api and return no of subscribers"""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-agent": "my-app/0.0.1"}
+    try:
+        responce = requests.get(url, headers=headers, allow_redirects=False)
+        if responce.status_code == 200:
+            data = responce.json()
+            return data['data']['subscribers']
+
+        else:
+            return 0
+
+    except requests.RequestException:
+        return 0
