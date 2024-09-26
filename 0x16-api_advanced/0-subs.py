3a0ffb6156faa6990e6499be95777e3f7d37d7fa
Peter Omondi <115159383+peterodero561@users.noreply.github.com>
2024-09-26 10:51:10 +0300

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
-"""Function to query Reddit API and return no of subscribers"""
+'''function to querry subreddit'''
 import requests
 
+
 def number_of_subscribers(subreddit):
-    """Query reddit api and return no of subscribers"""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-agent": "my-app/0.0.1"}
+    headers = {'User-Agent': 'MyBot/0.0.1'}
+
     try:
-        responce = requests.get(url, headers=headers, allow_redirects=False)
-        if responce.status_code == 200:
-            data = responce.json()
-            return data['data']['subscribers']
-
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            subscribers = data['data']['subscribers']
+            return subscribers
         else:
-            return 0
-
-    except requests.RequestException:
-        return 0
+            return 0  # Invalid subreddit or other error
+    except Exception as e:
+        print(f"Error: {e}")
+        return 0  # Return 0 if an exception occurs
